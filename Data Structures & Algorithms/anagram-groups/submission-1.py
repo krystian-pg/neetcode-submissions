c1e3bb1be8cade8396ffdec48ea5152196ac7eb2
Krystian Brzozowski <krybrzoz@pg.edu.pl>
2026-08-13 14:31:01 +0200

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,9 @@
+from collections import Counter, defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = defaultdict(list)
+        for string in strs:
+            key = tuple(sorted(string))
+            anagrams[key].append(string)
+        return list(anagrams.values())
